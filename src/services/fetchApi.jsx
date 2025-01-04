a07a6ebaf987243607387f9a62d1fc472a7fad43
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-04 22:15:46 +0700

    config file .env

--- src/services/fetchApi.jsx
+++ src/services/fetchApi.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3002";
+const apiUrl = process.env.REACT_APP_BASE_URL_API;
+
+axios.defaults.baseURL = apiUrl;
 axios.defaults.headers = {
   "Content-Type": "application/json",
 };
