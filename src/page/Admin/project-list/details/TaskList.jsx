fa016d846c08efa2ddd3d3d37f7a43de84d2c639
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-20 11:09:52 +0700

    style: ui task list (admin)

--- src/page/Admin/project-list/details/TaskList.jsx
+++ src/page/Admin/project-list/details/TaskList.jsx
@@ -1,7 +1,32 @@
+import { Avatar, Badge, Tooltip } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import React from "react";
+
 const TaskList = () => {
   return (
-    <>Task</>
-  )
-}
+    <div className="p-4">
+      <ul>
+        <li>
+          <Badge.Ribbon text="completed" color="green">
+            <div className="bg-red-50 border-l-4 border-red-500 rounded-lg p-4 shadow-md">
+              <h3 className="text-lg font-semibold text-red-700">Task name</h3>
+              <p className="text-sm text-gray-600">Deadline: 2025-02-25</p>
+              <div>
+                Staff:{" "}
+                <Tooltip title="Ant User">
+                  <Avatar
+                    size="small"
+                    style={{ backgroundColor: "#87d068" }}
+                    icon={<UserOutlined />}
+                  />
+                </Tooltip>
+              </div>
+            </div>
+          </Badge.Ribbon>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export default TaskList;
