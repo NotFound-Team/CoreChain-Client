4d87bfd7b9ed048ab2908905ac39e024dcaa7ef2
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-03-15 20:37:19 +0700

    config: file pretierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  bracketSameLine: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  tabWidth: 2,
+}
