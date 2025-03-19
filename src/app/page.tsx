0991ed3c2ca56b87ede254b196e68024a24b5151
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-03-19 22:48:57 +0700

    fix(header): scroll trasition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <>
       <PlatformIntro />
       <FeatureHighlights />
-      <Steps />
       <Pricing />
+      <Steps />
       <ButtonMode />
     </>
   );
