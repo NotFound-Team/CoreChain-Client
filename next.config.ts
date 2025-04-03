764fca643352e20831bba2e1b166e6a71c8ce8af
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-04-03 16:17:15 +0700

    config: turbo for environment develop

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos', 'another-domain.com'], // Thêm các tên miền khác nếu cần
+    domains: ["picsum.photos", "another-domain.com"], // Thêm các tên miền khác nếu cần
+  },
+  experimental: {
+    turbo: {
+      resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".json"],
+      rules: {
+        "*.mdx": ["mdx-loader"],
+        "*.jsx": ["babel-loader"],
+      },
+    },
   },
 };
 
