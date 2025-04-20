abb481b615efd3730876f0ebecf9f34b2a15415f
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-04-20 22:45:04 +0700

    fix: rename permission to role

---
+++ src/types/role.d.ts
@@ -0,0 +1,25 @@
+type Permission = {
+  piPath: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  module?: string;
+  name?: string;
+  _id: string;
+};
+
+type UpdatedBy = {
+  _id: string;
+  email: string;
+};
+
+export type Role = {
+  _id: string;
+  name: string;
+  description: string;
+  permissions: Permission[];
+  isActive: boolean;
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  updatedBy?: UpdatedBy;
+};
