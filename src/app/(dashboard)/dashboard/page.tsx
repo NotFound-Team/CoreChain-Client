db5e25d3856e470d348b60c0c546b336e8624c7a
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-04-22 00:54:44 +0700

    feat(dashboard): update ui page dashboard - fech api on server - optimize page load speed

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,29 @@
-export default function Dashboard() {
-  return (
-    <>
-      <h2 className="font-bold text-4xl">
-        WELLCOME DASHBOARD
-      </h2>
-    </>
-  );
+// app/dashboard/page.tsx
+import { cookies } from "next/headers";
+import { BASE_URL, CONFIG_API } from "@/configs/api";
+import DashboardClient from "./DashboardClient";
+
+export default async function DashboardPage() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const [projects, tasks, users] = await Promise.all([
+    fetch(`${BASE_URL}${CONFIG_API.PROJECT}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => res.json()),
+    fetch(`${BASE_URL}${CONFIG_API.TASK}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => res.json()),
+    fetch(`${BASE_URL}${CONFIG_API.USER.INDEX}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => res.json()),
+  ]);
+
+  return <DashboardClient data={{ projects, tasks, users }} />;
 }
