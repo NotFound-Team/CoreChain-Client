fed5e45963572a34b85c37e922a49eaf4e63356c
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-04-28 23:00:10 +0700

    add: add rootElement in commponent MUI

--- src/theme/ThemeOptions.ts
+++ src/theme/ThemeOptions.ts
@@ -2,10 +2,29 @@
 import { PaletteOptions } from "@mui/material";
 import createTheme from "@mui/material/styles/createTheme";
 
+const rootElement = typeof document !== "undefined" ? document.getElementById("root") : null;
+
 const ThemeOptions = (palette: PaletteOptions) => {
   return createTheme({
     palette: palette,
     cssVariables: true,
+    components: {
+      MuiPopover: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+      MuiPopper: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+      MuiDialog: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+    },
   });
 };
 
