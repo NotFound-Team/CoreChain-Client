1290e5122960f6be60c2f5cfd39ab1d38088405b
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2026-01-07 15:31:37 +0700

    feat: setup Husky, Lint-Staged, CommitLint

---
+++ commitlint.config.mjs
@@ -0,0 +1,16 @@
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "perf", "ci", "build", "revert"],
+    ],
+    "header-max-length": [2, "always", 150],
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+  },
+};
+
+// type(scope?): subject
+
+export default config;
